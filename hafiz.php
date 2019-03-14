d43e798a9b5761f88aa251a2deae93b08a0fdd24
hafiznuzula24 <31851870+hafiznuzula24@users.noreply.github.com>
2019-03-14 18:43:52 +0700

    Add files via upload

---
+++ hafiz.php
@@ -0,0 +1,10 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+   <?php echo '<p>Nama: Hafiz Nuzula</p>'; ?> 
+    <?php echo '<p>NIM: 14.01.53.0145</p>'; ?> 
+ </body>
+</html>
